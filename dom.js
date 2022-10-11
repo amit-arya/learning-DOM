de04c547686fb6828799bdc5b28d08c0ff71d024
amit-arya <amit3128arya@gmail.com>
2022-10-11 14:16:05 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -15,20 +15,24 @@ var header=document.getElementById('main-header');
 //headerTitle.style.borderBottom='solid 3px #000';
 //header.style.borderBottom='solid 3px #000';
 
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
 
-//items[1].textContent='Hello 2';
-//items[1].style.fontWeight='bold';
-//items[1].style.backgroundColor='yellow';
+// li[1].textContent='Hello 2';
+// li[1].style.fontWeight='bold';
+// li[1].style.backgroundColor='yellow';
 
-//for(let i=0;i<items.length;i++){
- //   items[i].style.backgroundColor='#f4f4f4';
-//}
+// for(let i=0;i<li.length;i++){
+//     li[i].style.backgroundColor='#f4f4f4';
+// }
 
-items[2].style.backgroundColor='green';
+//  li[2].style.backgroundColor='green';
 
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
-}
+// for(let i=0;i<li.length;i++){
+//     li[i].style.fontWeight='bold';
+// }
+
+// Queryselector
+var header=document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
