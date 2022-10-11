c46709a54518934e1709783f3711b7dff588fdd4
amit-arya <amit3128arya@gmail.com>
2022-10-11 17:00:28 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -34,5 +34,21 @@ var header=document.getElementById('main-header');
 // }
 
 // Queryselector
-var header=document.querySelector('#main-header');
-header.style.borderBottom = 'solid 4px #ccc';
+// var header=document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
+
+// let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor='green';
+
+// let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.color='white';
+
+//QuerySelectorAll
+let titles = document.querySelectorAll('.list-group-item');
+titles[1].style.color='green';
+
+let odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(let i=0;i<odd.length;i++){
+    odd[i].style.background='green';
+}
